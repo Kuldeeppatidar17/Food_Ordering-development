82a2b7c783ab779f2a257249eeb442bbea42f1ed
Kuldeeppatidar17 <patidarkuldeep982@gmail.com>
2024-08-10 16:52:14 +0530

    ....

--- Front End/foodordering/src/index.js
+++ Front End/foodordering/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './State/Store/store';
@@ -18,4 +18,4 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
+//reportWebVitals();
